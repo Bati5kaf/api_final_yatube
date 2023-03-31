f6b3dc960d3616042d6cdc4a2a9cabb6430a8c0b
EncodedB <artemnechepurenko2004@gmail.com>
2023-03-31 20:42:40 +0300

    Added basic functionality for Yatube API, added new README.md

---
+++ yatube_api/api/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAuthorOrReadOnly(BasePermission):
+    """
+    Проверка является ли пользователь автором комментари/поста
+    или же нет.
+    """
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            return request.user == obj.author
