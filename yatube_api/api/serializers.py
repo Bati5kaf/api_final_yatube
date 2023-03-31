d822e3f28ec5cc64d6d09971b76fc262fc6326d5
EncodedB <artemnechepurenko2004@gmail.com>
2023-03-31 20:42:34 +0300

    Added basic functionality for Yatube API, added new README.md

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
+from django.shortcuts import get_object_or_404
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -17,7 +18,49 @@ class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
+    post = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+    )
 
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    title = serializers.StringRelatedField(read_only=True)
+    description = serializers.StringRelatedField()
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        if self.context['request'].user == data['following']:
+            raise serializers.ValidationError('На себя подписываться нельзя')
+        if Follow.objects.filter(user=self.context['request'].user,
+                                 following=data['following']).exists():
+            raise serializers.ValidationError('Вы уже подписаны на '
+                                              'этого пользователя')
+        return data
+
+    user = serializers.SlugRelatedField(
+        slug_field='username', read_only=True
+    )
+
+    following = serializers.SlugRelatedField(
+        slug_field='username', required=True, queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+
+    def create(self, validated_data):
+        following = get_object_or_404(User,
+                                      username=validated_data['following']
+                                      )
+        return Follow.objects.create(user=self.context['request'].user,
+                                     following=following)
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -9,11 +9,33 @@ class Post(models.Model):
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts')
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='posts',
+        help_text='Группа, к которой будет относится пост',
+        verbose_name='Post group'
+    )
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
 
     def __str__(self):
         return self.text
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200,)
+    description = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True,)
+    created = models.DateTimeField(
+        'Дата создания',
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
@@ -24,3 +46,18 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    # кто подписывается
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower'
+    )
+    # на кого подписываются
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
